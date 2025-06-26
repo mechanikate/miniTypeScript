cb8b9f3c15a457a9ec8864a459f5a8fccd3bf45e
mechanikate <cjmatthy200@gmail.com>
2025-06-26 12:42:52 -0400

    stuff

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ let lastTime; // End of test/when test is completed, same format as firstTime/cu
 let disabled = false; // Disable inputting if true (Enter to reset is still allowed)
 let startLength = 0; // Initial length of allToType before we remove any characters
 let personalBest = -1; // PB words per minute
+let pbv = localStorage.getItem("pbs");
+let pbs;
+let testInProgress = false; // Is a test running?
+pbs = pbv ? JSON.parse(pbv) : [];
 let currentTestType = {
     type: "words",
     words: 10,
@@ -27,10 +31,53 @@ const firstBreak = function (aStr, bStr) {
     }
     return -1;
 };
+const addPB = (cpm, wpm, tt) => {
+    let pb = {
+        testType: tt,
+        cpm: cpm,
+        wpm: wpm
+    };
+    let pbVal = localStorage.getItem("pbs"); // get pb JSON as a string
+    if (!pbVal) { // if it's invalid/falsy
+        pbs = [pb]; // then make pbs into a list with just this pb,
+        localStorage.setItem("pbs", JSON.stringify(pbs)); // save the pb,
+        return; // and exit early
+    }
+    let ourPbId = `${tt.type}-${tt.words}-${tt.timer}`; // convert into a compatible format for comparing to other pb
+    let uniquePbIds = pbs.map(pbV => pbV.testType).map(ttV => `${ttV.type}-${ttV.words}-${ttV.timer}`); // convert all the other pbs into the same format
+    if (!uniquePbIds.includes(ourPbId)) { // if we don't have the type of test already,
+        pbs.push(pb); // add it to the pb list, 
+        localStorage.setItem("pbs", JSON.stringify(pbs)); // save the pbs,
+        return; // and exit early
+    }
+    let contrastingPbIndex = uniquePbIds.indexOf(ourPbId); // if it already has this pb somewhere (perhaps multiple times),
+    while (contrastingPbIndex != -1) { // repeat until all the pbs of the same type are gone
+        let contrastingPb = pbs[contrastingPbIndex]; // find the pb from the key
+        if (contrastingPb.cpm >= cpm) { // if our pb is worse, 
+            localStorage.setItem("pbs", JSON.stringify(pbs)); // save the pbs,
+            uniquePbIds[contrastingPbIndex] = ""; // overwrite this pb,
+            contrastingPbIndex = uniquePbIds.indexOf(ourPbId); // reassign the index (see top of while loop for why it's used)
+            continue; // and continue onto next iteration if necessary
+        }
+        pbs.splice(contrastingPbIndex, 1); // if our pb is better, remove the worse pb that needs to be replaced
+        uniquePbIds.splice(contrastingPbIndex, 1); // remove the corresponding entrance from the list of unique pb ids,
+        contrastingPbIndex = uniquePbIds.indexOf(ourPbId); // reassign the index (again, see top of while loop)
+    }
+    pbs.push(pb); // then, we can push our pb,
+    localStorage.setItem("pbs", JSON.stringify(pbs)); // and save
+};
+const clampIfUndefined = (v, clampTo) => (v == undefined || v == null || v == -1) ? clampTo : v; // clamp to clampTo if the value is unusable/invalid
+const findPB = (tt) => clampIfUndefined(pbs[pbs.map(pbV => pbV.testType).map(ttV => `${ttV.type}-${ttV.words}-${ttV.timer}`).indexOf(`${tt.type}-${tt.words}-${tt.timer}`)], { testType: currentTestType, cpm: -1, wpm: -1 }); // find a pb of TestType tt and get it as PersonalBest
 function generateDatasetDropdown() {
     let names = datasetList.map(e => e.name);
     let ids = datasetList.map(e => e.id);
     names.forEach((e, i) => {
+        /** Should end up looking like this (in datasetField div):
+         * <div class="display-text smaller-text">
+         * 		<input type="radio" value=1 name="datasetPicker" id="optionenglish1k" />
+         * 		<label class="big-label" for="optionenglish1k">english 1k</label>
+         * </div>
+         */
         let eleDiv = document.createElement("div");
         let eleInput = document.createElement("input");
         let eleLabel = document.createElement("label");
@@ -57,28 +104,35 @@ function generatePrompt(len = 10, datasetIndex) {
     document.getElementById("toType").value = allToType;
 }
 function getTestType() {
-    const params = (document.querySelector(`input[name="typePicker"]:checked`).value).split("-");
+    const params = (document.querySelector(`input[name="typePicker"]:checked`).value).split("-"); // from "words-10-0" to ["words", "10", "0"]
     return {
-        type: params[0],
-        words: parseInt(params[1]),
-        timer: parseInt(params[2])
+        type: params[0], // using above example: "words" (format title)
+        words: parseInt(params[1]), // using above example: 10 (words)
+        timer: parseInt(params[2]) // using above example: 0 (seconds/no time limit)
     };
 }
+const updatePB = () => {
+    personalBest = findPB(currentTestType).wpm;
+    document.getElementById("pbWordsPerMin").innerHTML = isNaN(personalBest) || personalBest === -1 ? "0.000" : personalBest.toFixed(3); // display 
+};
 function finishTest() {
     document.getElementById("typed").style.backgroundColor = "#4c9638"; // 3gIV. If we've finished, continue and set the background color to green to indicate the player is done
     lastTime = new Date().getTime(); // 3gV. Get the final time (see firstTime/currTime for what the value means)
     disabled = true; // 3gVI. Disable more inputting
     window.setTimeout(() => {
-        personalBest = Math.max(personalBest, currWpm);
-        document.getElementById("pbWordsPerMin").innerHTML = isNaN(personalBest) || personalBest === -1 ? "0.000" : personalBest.toFixed(3);
+        addPB(currCpm, currWpm, currentTestType);
+        updatePB();
     }, 125);
 }
 window.onload = function () {
+    personalBest = -1;
+    updatePB();
     generateDatasetDropdown();
     generatePrompt(10, 0); // 1. Generate the first prompt
     document.getElementById("typed").addEventListener("paste", e => e.preventDefault()); // 2. Disable cheating via pasting text
     Array.from(document.querySelectorAll(".type-picker-radio")).forEach((e) => e.addEventListener("click", e => {
         currentTestType = getTestType();
+        updatePB();
         reset();
     }));
     document.getElementById("typed").addEventListener("input", e => {
@@ -86,8 +140,10 @@ window.onload = function () {
             e.preventDefault();
             return;
         }
-        if (firstTime == undefined)
+        if (firstTime == undefined) {
+            testInProgress = true;
             firstTime = new Date().getTime(); // 3b. If we haven't started the stopwatch, start it
+        }
         let currentlyTyped = document.getElementById("typed").value; // 3c. Set what we've currently typed thus far (will be cleared later)
         nextIndex = currentlyTyped.length; // 3d. Assign nextIndex to the last character's index of currentlyTyped
         let shouldBeTyped = allToType.slice(0, nextIndex); // 3e. What do we need to type? For example, if we've typed "hello w" and we're to type "hello world" in total, this should be "orld"
@@ -105,7 +161,7 @@ window.onload = function () {
             return; // 3gVIII. Don't continue to the next part! 
         }
         console.log(`wrong @ ${firstBroken} (and maybe onwards)`); // 3h. If the player misinputted, log a little info in the console telling where we're wrong for debugging
-        document.getElementById("typed").style.color = "red"; // 3i. If the player misinputted, set the color of their inputted text to red to tell them they're wrong somewhereDropdown.
+        document.getElementById("typed").style.color = "red"; // 3i. If the  misinputted, set the color of their inputted text to red to tell them they're wrong somewhereDropdown.
     });
     window.setInterval(() => {
         currTime = lastTime === undefined ? new Date().getTime() : lastTime;
@@ -115,28 +171,33 @@ window.onload = function () {
         currWpm = currCpm / 5;
         if (currentTestType.timer > 0 && secs > currentTestType.timer && !disabled)
             finishTest();
-        updateDisplays(); // 4a. Also update the displays
+        if (testInProgress)
+            updateDisplays(); // 4a. Also update the displays
     }, 100);
     document.getElementById("typed").addEventListener("keypress", e => {
         if (e.key == "Enter") {
-            disabled = false;
             reset();
+            updateDisplays();
         } // 5a. If Enter has been pressed, reset to the start
         if (disabled) {
             e.preventDefault();
             return;
         } // 5b. Otherwise, make sure we're not typing if disabled is true.
     });
-    Array.from(document.getElementsByName("datasetPicker")).forEach(ele => ele.addEventListener("click", e => {
+    [...Array.from(document.getElementsByName("datasetPicker")), ...Array.from(document.getElementsByName("typePicker"))].forEach(ele => ele.addEventListener("click", e => {
         reset();
+        updatePB();
+        window.setTimeout(() => document.getElementById("timeLeft").innerHTML = currentTestType.timer.toFixed(1), 20); // make the time display the amount of time granted, delay 20ms to allow time for previous display updates to stop
     }));
 };
 function updateDisplays() {
     document.getElementById("charPerSecond").innerHTML = isNaN(currCpm) ? "0.000" : currCpm.toFixed(3); // Characters per minute (cpm), fix to 3 decimal places after the .
     document.getElementById("wordsPerMin").innerHTML = isNaN(currWpm) ? "0.000" : currWpm.toFixed(3); // Words per minute (wpm), fix to 3 decimal places after the .
-    document.getElementById("timeLeft").innerHTML = isNaN(currTime - firstTime) || currentTestType.timer == 0 ? "0.0" : (currentTestType.timer - (currTime - firstTime) / 1000).toFixed(1); // Time left (s), fix to 1 decimal place after the .
+    document.getElementById("timeLeft").innerHTML = isNaN(currTime - firstTime) || currentTestType.timer <= 0 ? "0.0" : Math.abs((currentTestType.timer - (currTime - firstTime) / 1000)).toFixed(1); // Time left (s), fix to 1 decimal place after the .
 }
 function reset() {
+    disabled = false; // Enable typing again
+    testInProgress = false; // Mark that we're done with this test
     currCpm = 0; // Reset current CPM counter
     currWpm = 0; // Reset current WPM counter
     generatePrompt(currentTestType.words, parseInt(document.querySelector(`input[name="datasetPicker"]:checked`).value)); // New prompt
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@
  * Welcome to miniTS!
  * This is a personal toy project to mess around with TypeScript for the first time.
  * Expect shenanigans and bugs.
+ * TODO:
+ * * more datasets
+ * * more lengths
+ * * custom bps for each dataset AND length, not just length
  * - matthy.dev
  */
 interface TestType {
 	type: string;
 	words: number;
 	timer: number;
+}
+interface PersonalBest {
+	testType: TestType;
+	cpm: number;
+	wpm: number;
 }
 let allToType: string = (<HTMLInputElement>document.getElementById("toType")).value; // The remainder of the text to type
 let nextIndex: number = 1; // The next index the user will type
@@ -19,7 +28,11 @@ let lastTime: number; // End of test/when test is completed, same format as firs
 let disabled: boolean = false; // Disable inputting if true (Enter to reset is still allowed)
 let startLength: number = 0; // Initial length of allToType before we remove any characters
 let personalBest: number = -1; // PB words per minute
-let currentTestType: TestType = {
+let pbv: string = localStorage.getItem("pbs");
+let pbs: PersonalBest[]; 
+let testInProgress: boolean = false; // Is a test running?
+pbs = pbv ? JSON.parse(pbv) : [];
+let currentTestType: TestType = { // default to words 10 (english100)
 	type: "words",
 	words: 10,
 	timer: 0
@@ -36,10 +49,55 @@ const firstBreak = function (aStr: string, bStr: string): number { // Calculates
 	}
 	return -1;
 };
-function generateDatasetDropdown() {
+const addPB = (cpm: number, wpm: number, tt: TestType) => {
+	let pb: PersonalBest = { // format the provided numbers into a PersonalBest object
+		testType: tt,
+		cpm: cpm,
+		wpm: wpm
+	};
+	let pbVal: string = localStorage.getItem("pbs"); // get pb JSON as a string
+	if(!pbVal) { // if it's invalid/falsy
+		pbs = [pb]; // then make pbs into a list with just this pb,
+		localStorage.setItem("pbs", JSON.stringify(pbs)); // save the pb,
+		return; // and exit early
+	}
+	let ourPbId: string = `${tt.type}-${tt.words}-${tt.timer}`; // convert into a compatible format for comparing to other pb
+	let uniquePbIds: string[] = pbs.map(pbV => pbV.testType).map(ttV => `${ttV.type}-${ttV.words}-${ttV.timer}`); // convert all the other pbs into the same format
+	if(!uniquePbIds.includes(ourPbId)) { // if we don't have the type of test already,
+		pbs.push(pb); // add it to the pb list, 
+		localStorage.setItem("pbs", JSON.stringify(pbs)); // save the pbs,
+		return; // and exit early
+	}
+	let contrastingPbIndex: number = uniquePbIds.indexOf(ourPbId); // if it already has this pb somewhere (perhaps multiple times),
+	while(contrastingPbIndex != -1) { // repeat until all the pbs of the same type are gone
+		let contrastingPb: PersonalBest = pbs[contrastingPbIndex]; // find the pb from the key
+		if(contrastingPb.cpm >= cpm) { // if our pb is worse, 
+			localStorage.setItem("pbs", JSON.stringify(pbs)); // save the pbs,
+			uniquePbIds[contrastingPbIndex] = ""; // overwrite this pb,
+			contrastingPbIndex = uniquePbIds.indexOf(ourPbId); // reassign the index (see top of while loop for why it's used)
+			continue; // and continue onto next iteration if necessary
+		}
+		pbs.splice(contrastingPbIndex, 1); // if our pb is better, remove the worse pb that needs to be replaced
+		uniquePbIds.splice(contrastingPbIndex, 1); // remove the corresponding entrance from the list of unique pb ids,
+		contrastingPbIndex = uniquePbIds.indexOf(ourPbId); // reassign the index (again, see top of while loop)
+	}
+	pbs.push(pb); // then, we can push our pb,
+	localStorage.setItem("pbs", JSON.stringify(pbs)); // and save
+};
+const clampIfUndefined = (v,clampTo) => (v == undefined || v == null || v == -1) ? clampTo : v; // clamp to clampTo if the value is unusable/invalid
+const findPB = (tt: TestType): PersonalBest => clampIfUndefined(pbs[pbs.map(pbV => pbV.testType).map(ttV => `${ttV.type}-${ttV.words}-${ttV.timer}`).indexOf(`${tt.type}-${tt.words}-${tt.timer}`)], {testType: currentTestType, cpm: -1, wpm: -1}); // find a pb of TestType tt and get it as PersonalBest
+ 
+
+function generateDatasetDropdown() { // make the dropdowns for languages/datasets (english1k, english100, etc.)
 	let names: string[] = datasetList.map(e => e.name);
 	let ids: string[] = datasetList.map(e => e.id);
 	names.forEach((e: string, i: number) => {
+		/** Should end up looking like this (in datasetField div):
+		 * <div class="display-text smaller-text">
+		 * 		<input type="radio" value=1 name="datasetPicker" id="optionenglish1k" />
+		 * 		<label class="big-label" for="optionenglish1k">english 1k</label>
+		 * </div>
+		 */
 		let eleDiv: HTMLElement = document.createElement("div");
 		let eleInput: HTMLInputElement = document.createElement("input");
 		let eleLabel: HTMLElement = document.createElement("label");
@@ -59,34 +117,41 @@ function generateDatasetDropdown() {
 		document.getElementById("datasetField").appendChild(eleDiv);	
 	});
 }
-function generatePrompt(len: number = 10, datasetIndex: number) { // Generate the 10 random words separated by spaces
+function generatePrompt(len: number = 10, datasetIndex: number) { // Generate len random words separated by spaces
 	allToType=datasetList[datasetIndex].generatorFunction(len);
 	startLength=allToType.length;
 	(<HTMLInputElement>document.getElementById("toType")).value=allToType;
 }
-function getTestType(): TestType {
-	const params: string[] = ((<HTMLInputElement>document.querySelector(`input[name="typePicker"]:checked`)).value).split("-");
+function getTestType(): TestType { // get active test type from value as TestType
+	const params: string[] = ((<HTMLInputElement>document.querySelector(`input[name="typePicker"]:checked`)).value).split("-"); // from "words-10-0" to ["words", "10", "0"]
 	return {
-		type: params[0],
-		words: parseInt(params[1]),
-		timer: parseInt(params[2])
+		type: params[0], // using above example: "words" (format title)
+		words: parseInt(params[1]), // using above example: 10 (words)
+		timer: parseInt(params[2]) // using above example: 0 (seconds/no time limit)
 	};
+}
+const updatePB = () => { // Update personal best value and display 
+	personalBest = findPB(currentTestType).wpm;
+	document.getElementById("pbWordsPerMin").innerHTML=isNaN(personalBest) || personalBest === -1 ? "0.000" : personalBest.toFixed(3); // display 
 }
 function finishTest() { // Ends the test
 	document.getElementById("typed").style.backgroundColor="#4c9638"; // 3gIV. If we've finished, continue and set the background color to green to indicate the player is done
 	lastTime = new Date().getTime(); // 3gV. Get the final time (see firstTime/currTime for what the value means)
 	disabled = true; // 3gVI. Disable more inputting
-	window.setTimeout(() => { // delay 125ms to make sure we get our last cpm/wpm update
-		personalBest = Math.max(personalBest, currWpm);
-		document.getElementById("pbWordsPerMin").innerHTML=isNaN(personalBest) || personalBest === -1 ? "0.000" : personalBest.toFixed(3); 
+	window.setTimeout(() => {
+		addPB(currCpm, currWpm, currentTestType);
+		updatePB();
 	}, 125);
 }
 window.onload = function() { // This is where a lot happens, so step-by-step
+	personalBest = -1;
+	updatePB();
 	generateDatasetDropdown();
 	generatePrompt(10,0); // 1. Generate the first prompt
 	document.getElementById("typed").addEventListener("paste", e => e.preventDefault()); // 2. Disable cheating via pasting text
 	Array.from(document.querySelectorAll(".type-picker-radio")).forEach((e: HTMLElement) => e.addEventListener("click", e => {
 		currentTestType = getTestType();
+		updatePB();
 		reset();
 	}));
 	document.getElementById("typed").addEventListener("input", e => { // 3. When we input, do a lot of things:
@@ -94,7 +159,10 @@ window.onload = function() { // This is where a lot happens, so step-by-step
 			e.preventDefault();
 			return;
 		}
-		if(firstTime == undefined) firstTime = new Date().getTime(); // 3b. If we haven't started the stopwatch, start it
+		if(firstTime == undefined) {
+			testInProgress = true;
+			firstTime = new Date().getTime(); // 3b. If we haven't started the stopwatch, start it
+		}
 		let currentlyTyped: string = (<HTMLInputElement>document.getElementById("typed")).value; // 3c. Set what we've currently typed thus far (will be cleared later)
 		nextIndex = currentlyTyped.length; // 3d. Assign nextIndex to the last character's index of currentlyTyped
 		let shouldBeTyped: string = allToType.slice(0, nextIndex); // 3e. What do we need to type? For example, if we've typed "hello w" and we're to type "hello world" in total, this should be "orld"
@@ -111,7 +179,7 @@ window.onload = function() { // This is where a lot happens, so step-by-step
 			return; // 3gVIII. Don't continue to the next part! 
 		}
 		console.log(`wrong @ ${firstBroken} (and maybe onwards)`); // 3h. If the player misinputted, log a little info in the console telling where we're wrong for debugging
-		document.getElementById("typed").style.color="red"; // 3i. If the player misinputted, set the color of their inputted text to red to tell them they're wrong somewhereDropdown.
+		document.getElementById("typed").style.color="red"; // 3i. If the  misinputted, set the color of their inputted text to red to tell them they're wrong somewhereDropdown.
 	});
 	window.setInterval(() => { // 4. Update the currCpm and currWpm counters every 100 milliseconds
 		currTime = lastTime === undefined ? new Date().getTime() : lastTime;
@@ -120,23 +188,26 @@ window.onload = function() { // This is where a lot happens, so step-by-step
 		currCpm = mins !== 0 ? (startLength - allToType.length)/mins : 0;
 		currWpm = currCpm/5;
 		if(currentTestType.timer > 0 && secs > currentTestType.timer && !disabled) finishTest(); 
-		updateDisplays(); // 4a. Also update the displays
+		if(testInProgress) updateDisplays(); // 4a. Also update the displays
 	}, 100);
 	document.getElementById("typed").addEventListener("keypress", e => { // 5. Add an additional listener to check if Enter is pressed
-		if(e.key == "Enter") { disabled = false; reset(); } // 5a. If Enter has been pressed, reset to the start
+		if(e.key == "Enter") {reset(); updateDisplays(); } // 5a. If Enter has been pressed, reset to the start
 		if(disabled) { e.preventDefault(); return; } // 5b. Otherwise, make sure we're not typing if disabled is true.
 	});
-	Array.from(document.getElementsByName("datasetPicker")).forEach(ele => ele.addEventListener("click", e => { // 6. Add another listener to reset the prompt if the player chooses another dataset
+	[...Array.from(document.getElementsByName("datasetPicker")), ...Array.from(document.getElementsByName("typePicker"))].forEach(ele => ele.addEventListener("click", e => { // 6. Add another listener to reset the prompt if the player chooses another dataset
 		reset();
+		updatePB();
+		window.setTimeout(() => document.getElementById("timeLeft").innerHTML=currentTestType.timer.toFixed(1), 20); // make the time display the amount of time granted, delay 20ms to allow time for previous display updates to stop
 	}));
 };
 function updateDisplays() { // Update the end displays below the input box
 	document.getElementById("charPerSecond").innerHTML=isNaN(currCpm) ? "0.000" : currCpm.toFixed(3); // Characters per minute (cpm), fix to 3 decimal places after the .
 	document.getElementById("wordsPerMin").innerHTML=isNaN(currWpm) ? "0.000" : currWpm.toFixed(3); // Words per minute (wpm), fix to 3 decimal places after the .
-	document.getElementById("timeLeft").innerHTML=isNaN(currTime-firstTime) || currentTestType.timer == 0 ? "0.0" : (currentTestType.timer-(currTime-firstTime)/1000).toFixed(1); // Time left (s), fix to 1 decimal place after the .
-
+	document.getElementById("timeLeft").innerHTML=isNaN(currTime-firstTime) || currentTestType.timer <= 0 ? "0.0" : Math.abs((currentTestType.timer-(currTime-firstTime)/1000)).toFixed(1); // Time left (s), fix to 1 decimal place after the .
 }
-function reset() { // RESET EVERYTHING excl. PB
+function reset() { // reset test, e.g. excl. PB
+	disabled = false; // Enable typing again
+	testInProgress=false; // Mark that we're done with this test
 	currCpm=0; // Reset current CPM counter
 	currWpm=0; // Reset current WPM counter
 	generatePrompt(currentTestType.words, parseInt((<HTMLInputElement>document.querySelector(`input[name="datasetPicker"]:checked`)).value)); // New prompt
